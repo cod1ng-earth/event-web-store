73963e94196a3595068487c2c42242d41723fb0f
David Sauer <david.sauer@turbinekreuzberg.com>
2019-04-18 17:39:16 +0200

    getting warm with 0.19

--- backend/cmd/collins/main.go
+++ backend/cmd/collins/main.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	log.Println("Hello, world.")
+	log.Println("Hello, world!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you have requested: %s\n", r.URL.Path)
+		t := time.Now()
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+		fmt.Fprintf(w, "Hello, you have requested: %s %d", r.URL.Path, t.UnixNano())
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,50 +1,152 @@
 module Main exposing (main)
 
+-- https://package.elm-lang.org/packages/NoRedInk/elm-sweet-poll/latest/
+-- https://package.elm-lang.org/packages/simonh1000/elm-jwt/latest/
+-- https://github.com/simonh1000/elm-jwt/blob/master/examples/node/src/Main.elm
+
 import Browser
-import Html exposing (Html, button, div, text)
+import Browser.Events exposing (onVisibilityChange, Visibility(..))
+import Html exposing (Html, button, div, text, h1)
 import Html.Events exposing (onClick)
+import Time
+import Task
+import Random
+import Http
+import Delay
 
 
 type alias Model =
-    { count : Int }
+    { count : Int
+    , same : Int
+    , text : String
+    , out : String
+    }
 
 
-initialModel : Model
-initialModel =
-    { count = 0  }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { count = 0
+      , same = 0
+      , text = ""
+      , out = ""
+      }
+    , Cmd.batch [ fetchResults ]
+    )
 
 
 type Msg
     = Increment
     | Decrement
+    | Tick Time.Posix
+    | Reload
+    | NewFace Int
+    | GotText (Result Http.Error String)
+    | VisibilityChange Browser.Events.Visibility
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Tick _ ->
+            ( model, Random.generate NewFace roll )
+
+        Reload ->
+            ( model, fetchResults )
+
+        NewFace role ->
+            if model.count == role then
+                ( { model | same = model.same + 1 }, Cmd.none )
+            else
+                ( { model | count = role }, Cmd.none )
+
         Increment ->
-            { model | count = model.count + 1 }
+            ( { model | count = model.count + 1 }, Cmd.none )
 
         Decrement ->
             if model.count > 0 then
-                { model | count = model.count - 1 }
+                ( { model | count = model.count - 1 }, Cmd.none )
             else
-                model
+                ( model, Cmd.none )
+
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( { model | text = fullText }, Delay.after 500 Delay.Millisecond Reload )
+
+                Err e ->
+                    ( { model | text = toString e }, Delay.after 500 Delay.Millisecond Reload )
+        
+        VisibilityChange Visible ->
+            ( { model | out = "v" }, Cmd.none )
+
+        VisibilityChange Hidden ->
+            ( { model | out = "h" }, Cmd.none )
+
+
+
+toString : Http.Error -> String
+toString error =
+    case error of
+        Http.BadUrl url ->
+            url ++ " is bad"
+
+        Http.Timeout ->
+            "Timeout"
+
+        Http.NetworkError ->
+            "Network Error"
+
+        Http.BadStatus status ->
+            String.fromInt status ++ " Status"
+
+        Http.BadBody msg ->
+            msg
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+        [ h1 [] [ text "Hello, world." ]
+        , div []
+            [ button [ onClick Increment ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count ]
+            , div [] [ text <| String.fromInt model.same ]
+            , button [ onClick Decrement ] [ text "-1" ]
+            , div [] [ text model.text ]
+            , div [] [ text model.out ]
+            ]
         ]
 
 
+roll : Random.Generator Int
+roll =
+    Random.int 1 6
+
+
+fetchResults : Cmd Msg
+fetchResults =
+    Http.get
+        { url = "http://localhost:8080/foobarbaz"
+        , expect = Http.expectString GotText
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ Time.every 1000 Tick
+        -- , Time.every 2000 Reload
+        , onVisibilityChange abc
+        ]
+
+abc : Browser.Events.Visibility -> Msg
+abc visibility = VisibilityChange visibility
+
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
