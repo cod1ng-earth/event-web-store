d45293579169f54d3b244f0c8749da10a835f179
David Sauer <david.sauer@turbinekreuzberg.com>
2019-08-05 18:39:50 +0200

    start kafka / connect to kafka from go

--- backend/cmd/collins/main.go
+++ backend/cmd/collins/main.go
@@ -5,9 +5,35 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/Shopify/sarama"
+	cluster "github.com/bsm/sarama-cluster"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	brokerList = kingpin.Flag("brokerList", "List of brokers to connect").Default("localhost:9092").Strings()
+	topic      = kingpin.Flag("topic", "Topic name").Default("products").String()
 )
 
 func main() {
+	kingpin.Parse()
+
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	config.Group.Return.Notifications = true
+	topics := []string{*topic}
+	consumer, err := cluster.NewConsumer(*brokerList, "inventory-categories-v1", topics, config)
+	if err != nil {
+		log.Panicf("failed to setup kafka consumer: %s", err)
+	}
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Panicf("failed to close kafka consumer: %s", err)
+		}
+	}()
+
 	log.Println("Hello, world!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
